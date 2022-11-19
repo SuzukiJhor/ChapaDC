64c4f2db5a53fc91919aa7ac49c194ee23026c5c
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-11-19 16:03:48 -0300

    create options

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ let meat = [
     'especial'
 ];
 
+let MeatPoint = ['Bem', 'Ponto', 'Mal']
 
-const getItem = event => {
-    insertSelect(event)
-    hideIcon(event)
-}
 
 const insertSelect = event => {
+    hideIcon(event)
     const parent = event.target.parentNode
     const item = document.querySelector(`.${parent.className.slice(5)}`)
     const selectOptions = createSelect()
@@ -27,8 +25,8 @@ const createSelect = () =>{
     select.id = 'myselect'
     select.name = 'myselect'
     select.setAttribute('onchange', 'insertMeat(this.value)')
-    const options =  createOptions(select)
-    return options
+    const selectOptions =  createOptions(select)
+    return selectOptions
 }
 
 const createOptions = select =>{
@@ -40,57 +38,105 @@ const createOptions = select =>{
 
 const hideIcon = meat =>{
     const icon = meat.target
-    const iconClass = meat.target.className
+    const iconClass = icon.className
     if(iconClass == 'fa-solid fa-play'){
         icon.style.display = 'none'
     }
 }
 
-const insertMeat = meat =>{
-
+const insertMeat = value =>{
+    
     container.addEventListener('click', e =>{
     const parentDiv = e.target.parentNode
-    insertDisplay()
-    // parentDiv.innerHTML = createTimerDiv()
+    const selectCurrent = parentDiv.querySelector('#myselect')
+    parentDiv.removeChild(selectCurrent)
+    dynamicContent(value,parentDiv) 
   })
   
 }
 
-const insertDisplay = ()=>{
-    const timerDiv = createTimerDiv()
-    console.log(timerDiv);
-    container.appendChild(timerDiv)
+const dynamicContent = (value, currentDiv) => {
+    
+    const displayShow = document.createElement('div')
+    displayShow.id = 'elementShow'
+
+    const textValue = document.createElement('p')
+    textValue.innerText = value
+    
+    displayShow.append(textValue)
+
+    currentDiv.appendChild(displayShow)
+
+    createPointTimer(currentDiv)
 }
 
-const createTimerDiv = ()=>{
+
+const createPointTimer = currentDiv =>{
+    const select = document.createElement('select')
+    select.id = 'selectPointMeat'
+    select.name = 'selectPointMeat'
+    select.setAttribute('onchange', 'getPoint(this.value)')
+    MeatPoint.forEach(item =>{
+        select.options[select.options.length] = new Option(`${item}`, `${item}`)
+    })
+    currentDiv.append(select)
+}
+
+const getPoint = point =>{
+    let timerPoint;
+
+    if( point == 'mal'){
+        timerPoint = 10
+    }
+    if( point=='ponto'){
+        timerPoint = 20
+    }
+    if(point = 'bem'){
+        timerPoint = 30
+    }
+
+    container.addEventListener('click', e =>{
+        const currentDiv = e.target.parentNode
+        const selectCurrent = currentDiv.querySelector('#selectPointMeat')
+        currentDiv.removeChild(selectCurrent)
+
+        createTimerDiv(timerPoint, currentDiv)
+})
+    
+}
+
+const createTimerDiv = (timer, currentDiv) =>{
     const timerDiv = document.createElement('div')
     timerDiv.id = 'timer'   
-    const duration = 60 * 3
+    const duration = timer
     startTimer(duration, timerDiv)
-    return timerDiv
+    currentDiv.appendChild(timerDiv)
+    console.log(currentDiv)
 }
 
 const startTimer = (duration, display)=>{
     let timer = duration, minutes, seconds
 
-    setInterval(()=>{
+    const meuInterval = setInterval(()=>{
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10)
 
         minutes = minutes < 10 ? '0'+ minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
 
-        console.log(minutes)
-
         display.textContent = minutes + ":" + seconds
-
+      
         if(--timer < 0){
-            timer = duration
+            timer = 0
         }
 
     },1000)
+
+    if(timer == 0){
+        clearInterval(meuInterval)
+    }
 }
 
-container.addEventListener('click', getItem)
+container.addEventListener('click', insertSelect)
 
- 
+
